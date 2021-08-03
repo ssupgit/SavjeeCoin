75eb0948124784728bc41a9fd344000624c140c8
ssup <sahabatstartup@gmail.com>
2021-08-03 20:56:22 +0700

    sorry

--- src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-
-mongoose.connect('mongodb://localhost:27017/blockchain',{useNewUrlParser:true});
-const db = mongoose.connection;
-console.log(`MongoDB Connected`);
-module.exports = {mongoose}
